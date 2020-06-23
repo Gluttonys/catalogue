e774dc76d1dc39ce4f467e19976cdb30ddee568d
Gluttonys <1720874194@qq.com>
2020-06-23 10:15:38 +0800

    初始化项目

---
+++ readme.md
@@ -0,0 +1,60 @@
+> 自动生成个人博客或文章的目录
+
+---
+
+1. 使用方法， 克隆项目到本地， 解压文件夹。
+
+    - 在头部引入CSS文件
+    
+    ```html
+    <link rel="stylesheet" href="your/path/catalogue.css">
+    ```
+    
+    - 自定义其位置， 默认位置 ` right: 40px ` , `bottom: 40px`
+    ```css
+    #catalogue {
+          bottom: 40px;
+          right: 40px;
+    }
+    ```
+    
+    - 在页面需要的地方添加HTML代码
+    ```html
+    <div id="catalogue"></div>
+    ```
+    
+    - 在尾部引入JS文件
+    ```html
+    <script src="your/path/catalogue.js"></script>    
+    ```
+    - 自定义样式
+    
+        - **下面的代码需要catalogue.js支持， 请务必放在其文件下方**
+    ```javascript
+    new Catalogue({
+        wrapper: "#content",
+        topLeave: "h1",
+        secondLeave: "h2",
+        backgroundColor: "#eee",
+        padding: "10",
+        color: "green",
+        fontSize: "16"
+      })
+    ```
+    
+2. 参数解释
+    
+    - **wrapper** : 这个参数是必须的。确定选择生成目录的边界, 接收一个选择器字符串, 该选择器不能包含目录按钮。默认为空 
+    - topLeave: 选择一级目录的标签。只能是 h1 ~ h6 。默认为 h1
+    - secondLeave:  选择二级目录的标签。只能是 h1 ~ h6 。默认为 h2
+    - backgroundColor： 目录面板的背景颜色， 接收任何合法的颜色值。 默认为 "#ec407a10"
+    - padding： 目录面板的内边距， 接收一个**数字类型字符串**。 默认为 16px
+    - color: 目录面板的字体颜色， 接收任何合法的颜色值。 默认为 "#EC407A"
+    - fontSize: 目录面板的字体大小， 接收一个**数字类型字符串**。 默认为 16px
+    
+3. 注意的地方
+    - 该脚本会给设置为目录的标签添加id属性。如果您有在设置为目录标签上设置id, 为了避免冲突， 请自行调试代码。
+    
+4. 优缺点
+    - 优点 ： 使用简单方便， 可自定义。 插件使用体积小，未压缩前两个文件共5.4Kb。
+    - 缺点 ： 缺少动画等过渡效果， 看起来比较生硬。 只支持两级目录， 暂不支持更多
---
+++ src/js/catalogue.js
@@ -0,0 +1,110 @@
+class Catalogue {
+
+  defaultOptions = {
+    wrapper: "",
+    topLeave: "h1",
+    secondLeave: "h2",
+    backgroundColor: "#ec407a10",
+    padding: "16",
+    color: "#EC407A",
+    fontSize: "16"
+  }
+
+  titleList = ["h1", "h2", "h3", "h4", "h5", "h6"]
+
+  constructor(options) {
+    // 展开按钮
+    this.button = document.getElementById("catalogue")
+    this.button.innerHTML = "<span>＋</span>"
+    // 目录页面
+    this.catalogueList = this.initCatalogueList()
+    // 初始化选项
+    this.defaultOptions = Object.assign(this.defaultOptions, options)
+
+    // 内容检错
+    if (!this.titleList.includes(this.defaultOptions.topLeave.toLowerCase()) ||
+      !this.titleList.includes(this.defaultOptions.secondLeave.toLowerCase())) {
+      throw new Error("检查 options 中 topLeave , secondLeave 属性， 请从 h1 , h2 , h3 , h4 , h5 , h6 中任选一个")
+    }
+
+    if (!this.defaultOptions.wrapper) throw new Error("options.wrapper 参数是必须的 ！")
+
+    // 初始化目录样式
+    this.initStyle()
+    // 填充列表目录
+    this.padding()
+  }
+
+  initStyle() {
+    /**
+     * 初始化目录页
+     * 1 ： 移入移出动画
+     * 2 ： 确定目录页位置
+     * 3 : 初始化用户自定义样式
+     */
+    let that = this
+
+    function moveIn() {
+      that.catalogueList.style.display = "block"
+    }
+
+    function moveOut() {
+      that.catalogueList.style.display = "none"
+    }
+
+    this.button.addEventListener("mouseenter", moveIn)
+    this.button.addEventListener("mouseleave", moveOut)
+    this.catalogueList.addEventListener("mouseenter", moveIn)
+    this.catalogueList.addEventListener("mouseleave", moveOut)
+
+    // 初始化目录位置
+    let computedStyle = window.getComputedStyle(this.button)
+    let positionRight = computedStyle["right"]
+    let positionBottom = computedStyle["bottom"]
+
+    let catalogueListStyle = this.catalogueList.style
+    catalogueListStyle.right = `${parseInt(positionRight) + 15}px`
+    catalogueListStyle.bottom = `${parseInt(positionBottom) + 15}px`
+    catalogueListStyle.backgroundColor = this.defaultOptions.backgroundColor
+    catalogueListStyle.padding = `${parseInt(this.defaultOptions.padding)}px`
+    catalogueListStyle.color = this.defaultOptions.color
+  }
+
+  initCatalogueList() {
+    /**
+     * 初始化目录页
+     * @type {HTMLElement}
+     */
+    let block = document.createElement("div")
+    block.id = "catalogue-list"
+    return this.button.insertAdjacentElement("afterend", block)
+  }
+
+  padding() {
+    let boundary = document.querySelector(this.defaultOptions.wrapper)
+    if (!boundary) throw new Error("当前选择器匹配不到任何元素。 请指定一个合法的选择器 ！！")
+
+    let resultStr = "<ol>"
+
+    let count = 0
+    for (let top of boundary.querySelectorAll("*")) {
+      top.id = `catalogue-${count}`
+      if (top.tagName === this.defaultOptions.topLeave.toUpperCase()) {
+        resultStr += `<li><a href="#catalogue-${count}">${top.innerText}</a></li>`
+      } else if (top.tagName === this.defaultOptions.secondLeave.toUpperCase()) {
+        resultStr += `<ul><li><a href="#catalogue-${count}">${top.innerText}</a></li></ul>`
+      }
+      count++
+    }
+
+    resultStr += "</ol>"
+    this.catalogueList.insertAdjacentHTML("afterbegin", resultStr)
+
+    for (let a of this.catalogueList.querySelectorAll("a")) {
+      a.style.color = this.defaultOptions.color
+      a.style.fontSize = `${parseInt(this.defaultOptions.fontSize)}px`
+    }
+
+  }
+
+}
